4d67344af61f75788d56119d0dbea362091eabbd
Abhishek <abhishekt2024@gmail.com>
2020-12-05 02:16:17 +0530

    more questions

---
+++ dynamic programming/coinCombinations1.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define INF (unsigned)!((int)0)
+#define m 1000000007
+#define fo(i, n) for(ll i=0;i<n;i++)
+typedef vector<int> vi;
+typedef vector<ll> vl;
+
+int main(){
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+    srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+    
+    return 0;
+}
