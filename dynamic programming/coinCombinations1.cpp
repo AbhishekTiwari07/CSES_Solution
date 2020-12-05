453bbcdb1a249f85763c78ae48790e2610409ae2
Abhishek <abhishekt2024@gmail.com>
2020-12-06 00:40:34 +0530

    more questions

--- dynamic programming/coinCombinations1.cpp
+++ dynamic programming/coinCombinations1.cpp
@@ -11,5 +11,27 @@ int main(){
     ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     srand(chrono::high_resolution_clock::now().time_since_epoch().count());
     
+    ll n,total,temp;
+    vl v,c;
+    cin>>n>>total;
+    fo(i,n){
+        cin>>temp;
+        c.push_back(temp);
+    }
+    fo(i,total+1){
+        temp=0;
+        if(i==0)
+            v.push_back(1);
+        else{
+            for(auto x:c){
+                if(i-x>=0){
+                    temp+=v[i-x];
+                    temp%=m;
+                }
+            }
+            v.push_back(temp);
+        }
+    }
+    cout<<v[total];
     return 0;
 }
